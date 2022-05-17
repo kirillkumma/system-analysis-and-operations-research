fcb4276f04c80f2e4abd5d5c44867d6d1956fcf4
Kirill Kumma <kirill.kumma@gmail.com>
2022-05-17 14:52:15 +0300

    format code

--- lab1/pulp/main.py
+++ lab1/pulp/main.py
@@ -4,14 +4,14 @@ from tkinter import messagebox
 
 win = tk.Tk()
 
-win.title('Максимизация прибыли предприятия')
-win.geometry('300x300')
+win.title("Максимизация прибыли предприятия")
+win.geometry("300x300")
 
-tk.Label(win, text='Цена A').grid(row=0)
-tk.Label(win, text='Цена B').grid(row=1)
-tk.Label(win, text='Максимальное кол-во A').grid(row=2)
-tk.Label(win, text='Максимальное кол-во B').grid(row=3)
-tk.Label(win, text='Пропускная способность').grid(row=4)
+tk.Label(win, text="Цена A").grid(row=0)
+tk.Label(win, text="Цена B").grid(row=1)
+tk.Label(win, text="Максимальное кол-во A").grid(row=2)
+tk.Label(win, text="Максимальное кол-во B").grid(row=3)
+tk.Label(win, text="Пропускная способность").grid(row=4)
 
 a_price = tk.Entry(win)
 b_price = tk.Entry(win)
@@ -32,11 +32,11 @@ def solve():
     a_max_count_val = int(a_max_count.get())
     b_max_count_val = int(b_max_count.get())
     bandwidth_val = int(bandwidth.get())
-    model = p.LpProblem('Максимальная прибыль', p.LpMaximize)
-    
-    A = p.LpVariable('1', lowBound=0, cat='integer')
-    B = p.LpVariable('2', lowBound=0, cat='integer')
-    
+    model = p.LpProblem("Максимальная прибыль", p.LpMaximize)
+
+    A = p.LpVariable("1", lowBound=0, cat="integer")
+    B = p.LpVariable("2", lowBound=0, cat="integer")
+
     model += a_price_val * A + b_price_val * B
     model += A >= 0
     model += A <= a_max_count_val
@@ -45,10 +45,11 @@ def solve():
     model += A + B <= bandwidth_val
 
     model.solve()
-    
+
     messagebox.showinfo(
-            'Ответ', 
-            f'A = {A.varValue}\nB = {B.varValue}\nMax = {a_price_val * A.varValue + b_price_val * B.varValue}')
+        "Ответ",
+        f"A = {A.varValue}\nB = {B.varValue}\nMax = {a_price_val * A.varValue + b_price_val * B.varValue}",
+    )
 
 
 btn = tk.Button(win, text="Решить", width=10, height=5, command=solve)
